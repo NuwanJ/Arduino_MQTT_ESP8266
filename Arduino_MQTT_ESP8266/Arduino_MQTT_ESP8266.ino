7b740eeb04bb1b20239d236b0b7152be600c4b37
saba007 <sabareesan2@gmail.com>
2018-03-21 11:33:32 +0530

    Add files via upload

---
+++ Arduino_MQTT_ESP8266/Arduino_MQTT_ESP8266.ino
@@ -0,0 +1,14 @@
+
+//Device Details
+String DEVICE_BRAND              = "ideamart";
+String DEVICE_TYPE               = "batmobile";
+String DEVICE_VERSION            = "v1";
+String DEVICE_SERIAL             = "427260235432";
+
+//Your Wifi Credentials--
+const char* ssid                 = "virus";
+const char* password             = "vtecvirus";
+
+//Dialog MQTT server Credentials
+const char* server_username      = "rabbit";
+const char* server_password      = "rabbit";
