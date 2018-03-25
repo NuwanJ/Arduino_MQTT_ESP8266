c6dd896923560faa5a874f31ec23ff8e637f616b
NuwanJ <nuwanjaliyagoda@gmail.com>
2018-03-25 10:34:24 +0530

    re-organize the code

---
+++ Arduino_MQTT_ESP8266/defines.h
@@ -0,0 +1,14 @@
+
+//Device Details
+const String DEVICE_BRAND              = "ideamart";
+const String DEVICE_TYPE               = "batmobile";
+const String DEVICE_VERSION            = "v1";
+const String DEVICE_SERIAL             = "427260235432";
+
+//Your Wifi Credentials--
+const char* ssid                 = "virus";
+const char* password             = "vtecvirus";
+
+//Dialog MQTT server Credentials
+const char* server_username      = "rabbit";
+const char* server_password      = "rabbit";
--- Arduino_MQTT_ESP8266/your_actions_events.ino
+++ Arduino_MQTT_ESP8266/your_actions_events.ino
@@ -1,39 +1,39 @@
 void do_actions(const char* message) {
   //Create this function according to your actions. you will receive a message something like this
 
-Serial.println(message);
-   
-StaticJsonBuffer<200> jsonBuffer;
+  Serial.println(message);
 
-JsonObject& root = jsonBuffer.parseObject(message);
+  StaticJsonBuffer<200> jsonBuffer;
 
-String action = root["action"]; // "power"
+  JsonObject& root = jsonBuffer.parseObject(message);
 
-JsonObject& param = root["param"]; 
-String param_mac = param["mac"]; // "1902100379354041"
+  String action = root["action"]; // "power"
 
-  if(param_mac.equals(DEVICE_SERIAL)){
-    if(action == "sweep"){
-        int pos = 0;    // variable to store the servo position
-        Servo myservo;
-        myservo.attach(4);
+  JsonObject& param = root["param"];
+  String param_mac = param["mac"]; // "1902100379354041"
+
+  if (param_mac.equals(DEVICE_SERIAL)) {
+    if (action == "sweep") {
+      int pos = 0;    // variable to store the servo position
+      
+      myservo.attach(4);
 
       for (pos = 0; pos <= 160; pos += 20) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
         delay(1000);                       // waits 15ms for the servo to reach the position
-        }
+      }
       for (pos = 180; pos >= 0; pos -= 20) { // goes from 180 degrees to 0 degrees
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
         delay(1000);                       // waits 1s for the servo to reach the position
-        }
+      }
       myservo.write(0);              // tell servo to go to position in variable 'pos'
 
-    } 
-    if(action == "off"){
-    analogWrite(4,0);
+    }
+    if (action == "off") {
+      analogWrite(4, 0);
 
-    } 
+    }
   }
 }
 //====================================================================================================================================================================
